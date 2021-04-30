7877b7837201773268a56a262a339e5c441d0173
matcls <manueltorresvesga@gmail.com>
2021-04-29 22:00:18 -0500

    Add C funtion that insert in a sorted linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts node into linked list
+ * in sorted order
+ *
+ * @head: pointer to head of list
+ * @number: the number to insert
+ *
+ * Return: pointer to new node or NULL on fail
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (!temp)
+		return (NULL);
+	temp->n = number;
+
+	if (!*head || number < (*head)->n)
+	{
+		temp->next = *head;
+		*head = temp;
+		return (temp);
+	}
+	for (node = *head; node->next && node->next->n <= number; node = node->next)
+		;
+	temp->next = node->next;
+	node->next = temp;
+	return (temp);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,18 @@
+# 0x01. Insert in sorted linked list
+
+## Description
+Technical interview preparation:
+
+- Write a function in C that inserts a number into a sorted singly linked list.
+
+		- Prototype: listint_t *insert_node(listint_t **head, int number);
+		- Return: the address of the new node, or NULL if it failed
+## Usage
+Compile and run
+
+	gcc -Wall -Werror -Wextra -pedantic 0-main.c linked_lists.c 0-insert_number.c -o insert
+	./insert
+---
+
+## Author
+* **Manuel Torres Vesga** - [matcls](https://github.com/matcls)
