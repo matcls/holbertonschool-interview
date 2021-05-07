27dcaf5ff0a82e37a6b0ab3636be95cdaa131039
matcls <manueltorresvesga@gmail.com>
2021-05-06 19:08:59 -0500

    Ensure memory assigment for the node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	/*Assign memory*/
-	/*memset(node, 0, sizeof(*node));*/
+	memset(node, 0, sizeof(*node));
 	node->parent = parent;
 	node->n = value;
 	return (node);
