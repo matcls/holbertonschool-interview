e33517e2e17dee7d58460bcd42733e4bc1a82ee6
Manuel Torres <manueltorresvesga@gmail.com>
2021-06-24 22:22:29 -0500

    Add docstrings

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""Determines if a given data set represents a valid UTF-8 encoding."""
+
+
 def validUTF8(data):
+    """Determine if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): list of integers
+
+    Returns:
+        Boolean: True if data is a valid UTF-8 encoding, else False
+    """
     n_bytes = 0
     for num in data:
         byte = num & 0xFF
